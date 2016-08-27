24fddc0d5b304674dc02386458793b3af2ae7912
Rogério Fernando Dometerco <rogeriodometerco@hotmail.com>
2016-08-26 23:39:02 -0300

    Recepção de evento do celular.

--- viagem/src/rest/MobileRest.java
+++ viagem/src/rest/MobileRest.java
@@ -1,5 +1,12 @@
 package rest;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -12,7 +19,9 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import dao.DispositivoDao;
 import dao.LocalizacaoDao;
+import dao.MotoristaDao;
 import modelo.Localizacao;
 import servico.LocalizacaoService;
 import util.Ejb;
@@ -22,16 +31,20 @@ public class MobileRest {
 
 	private LocalizacaoService localizacaoService;
 	private LocalizacaoDao localizacaoDao; // para testar funcionalidades que não estarão no service.
+	private MotoristaDao motoristaDao; // para testar funcionalidades que não estarão no service.
+	private DispositivoDao dispositivoDao; // para testar funcionalidades que não estarão no service.
 
 	public MobileRest() {
 		localizacaoService = Ejb.lookup(LocalizacaoService.class);
 		localizacaoDao = Ejb.lookup(LocalizacaoDao.class);
+		motoristaDao = Ejb.lookup(MotoristaDao.class);
+		dispositivoDao = Ejb.lookup(DispositivoDao.class);
 	}
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response registrar(@Context HttpServletRequest httpServletRequest, 
+	public Response testar(@Context HttpServletRequest httpServletRequest, 
 			Localizacao localizacao) throws Exception {
 		return Response.ok()
 				.entity(
@@ -39,7 +52,57 @@ public class MobileRest {
 						.registrar(localizacao))
 				.build();
 	}
+	
+	@POST
+	@Path("/evento")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response registrar(@Context HttpServletRequest httpServletRequest, 
+			Map<String, Object> request) throws Exception {
 
+/*
+ * {
+ * 	"events":[{
+ * 		"type":"LocaLizacaoEventoMobile",
+ * 		"altitude":10,
+ * 		"precisao":10,
+ * 		"provedor":"GPS",
+ * 		"velocidade":0,
+ * 		"dhEvento":"Aug 26, 2016 9:29:29 PM",
+ * 		"id":1,
+ * 		"localizacao":{
+ * 			"latitude":"29031920",
+ * 			"longitude":"-213123"}
+ * 		}
+ * 	]
+ * }
+ */
+		List<Map<String, Object>> eventos = (List<Map<String, Object>>)request.get("events");
+		Localizacao localizacao = null;
+		DateFormat format = new SimpleDateFormat("MMM dd, yyyy HH:mm:ss a", Locale.US);
+
+		for (Map<String, Object> evento: eventos) {
+			Map<String, Object> localizacaoRequest = (Map<String, Object>)evento.get("localizacao");
+			localizacao = new Localizacao();
+			localizacao.setData(format.parse((String)evento.get("dhEvento")));
+			localizacao.setLat((Double)localizacaoRequest.get("lat"));
+			localizacao.setLng((Double)localizacaoRequest.get("lng"));
+			localizacao.setVelocidade(Double.valueOf((Integer)evento.get("velocidade")));
+			
+			localizacao.setMotorista(motoristaDao.listar().get(0));
+			localizacao.setDispositivo(dispositivoDao.listar().get(0));
+			
+			localizacaoService.registrar(localizacao);
+		}
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("Status", "OK");
+		return Response.ok()
+				.entity(result)
+				.build();
+	}
+
+
+	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response listar()  throws Exception {
--- viagem/src/teste/Teste.java
+++ viagem/src/teste/Teste.java
@@ -1,42 +1,16 @@
 package teste;
 
-import java.util.Properties;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class Teste {
 
-	public static void main(String[] args) {
-		try {
+	public static void main(String[] args) throws Exception {
 
-			Properties jndiProperties = new Properties();
-			jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-			jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-			jndiProperties.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
-
-				Context jndi = new InitialContext(jndiProperties);
-				jndi.lookup("java:global/EventoService");
-
-//				Properties jndiProps = new Properties();
-//				jndiProps.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-				//jndiProps.put(Context.PROVIDER_URL,"remote://localhost:4447");
-				//Context ctx = new InitialContext(jndiProps);
-				Context ctx = new InitialContext();
-				// lookup
-				ctx.lookup("java:global/EventoService");
-
-				//final Hashtable jndiProperties = new Hashtable();
-				jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-				final Context context = new InitialContext(jndiProperties);
-				context.lookup("java:module/EventoService");
-				//Ejb.lookup(servico.LocalizacaoService.class);
-				//EventoService eventoService = Ejb.lookup(EventoService.class);
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
-		}
-
+		String dataStr = "Aug 26, 2016 9:29:29 PM";
+		DateFormat format = new SimpleDateFormat("MMM dd, yyyy HH:mm:ss a", Locale.US);
+		System.out.println(format.parse(dataStr));
+		
 	}
+}
