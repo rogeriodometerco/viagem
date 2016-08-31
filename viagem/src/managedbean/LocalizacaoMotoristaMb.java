af77b7c1f2f27d4acc17458c0a69cdfc082c69fd
Rogério Fernando Dometerco <rogeriodometerco@hotmail.com>
2016-08-30 22:48:03 -0300

    .

--- viagem/src/managedbean/LocalizacaoMotoristaMb.java
+++ viagem/src/managedbean/LocalizacaoMotoristaMb.java
@@ -6,6 +6,11 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.LatLng;
+import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Marker;
+
 import modelo.LocalizacaoMotorista;
 import servico.LocalizacaoMotoristaService;
 
@@ -15,15 +20,32 @@ public class LocalizacaoMotoristaMb {
 
 	@EJB
 	private LocalizacaoMotoristaService localizacaoMotoristaService;
-
+	private MapModel mapModel;
 	private List<LocalizacaoMotorista> localizacoes;
 
 	public List<LocalizacaoMotorista> getLocalizacoes() throws Exception {
-		if (localizacoes == null) {
-			localizacoes = localizacaoMotoristaService.listar();
-		}
 		return localizacoes;
 	}
 
+	public String getNome() {
+		return "rogerio";
+	}
+	public void listar() throws Exception {
+		localizacoes = localizacaoMotoristaService.listar();
+	}
+
+	public MapModel getMapModel() {
+		if (mapModel == null) {
+			mapModel = new DefaultMapModel();
+		}
+		if (localizacoes != null) {
+			mapModel.getMarkers().clear();
+			for (LocalizacaoMotorista localizacao: localizacoes) {
+				LatLng latLng = new LatLng(localizacao.getLat(), localizacao.getLng());
+				mapModel.addOverlay(new Marker(latLng, localizacao.getMotorista().getNome()));
+			}
+		}
+		return mapModel;
+	}
 
 }
